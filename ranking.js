5a27fb00a7164b4e0a7413394f4f9f9cf5d38478
lteruya <quemossabe@gmail.com>
2023-07-22 19:59:46 -0300

    inicio estudy javascript

---
+++ ranking.js
@@ -0,0 +1,304 @@
+
+var listGame = [
+    {
+        "id": "n0rLlEm14o",
+        "ranking": "OiCdafywDg",
+        "gameNumber": 1,
+        "account": "quemossabe@gmail.com",
+        "player1": {
+            "id": "QhiNkz9svJ",
+            "name": "CAZUO MATSUMOTO",
+            "clubId": "9wFZEy1KXt"
+        },
+        "player1Set": 3,
+        "player2": {
+            "id": "yP8fPpcud7",
+            "name": "ALEXIS LEBRUN",
+            "clubId": "9wFZEy1KXt"
+        },
+        "player2Set": 1,
+        "finished": true,
+        "startTime": 1689858923,
+        "finalTime": 1689858942
+    },
+    {
+        "id": "FRDnvuCRAQ",
+        "ranking": "OiCdafywDg",
+        "gameNumber": 2,
+        "account": "quemossabe@gmail.com",
+        "player1": {
+            "id": "CbBhnZnfSp",
+            "name": "TIMO BOLL",
+            "clubId": "ULCitbCiOX"
+        },
+        "player1Set": 3,
+        "player2": {
+            "id": "asgmFhR4ta",
+            "name": "MA LIN",
+            "clubId": "xbEoyZqpZ9"
+        },
+        "player2Set": 0,
+        "finished": true,
+        "startTime": 1689858928,
+        "finalTime": 1689858936
+    },
+    {
+        "id": "Fx1vhahOCQ",
+        "ranking": "OiCdafywDg",
+        "gameNumber": 3,
+        "account": "quemossabe@gmail.com",
+        "player1": {
+            "id": "CbBhnZnfSp",
+            "name": "TIMO BOLL",
+            "clubId": "ULCitbCiOX"
+        },
+        "player1Set": 3,
+        "player2": {
+            "id": "yP8fPpcud7",
+            "name": "ALEXIS LEBRUN",
+            "clubId": "9wFZEy1KXt"
+        },
+        "player2Set": 0,
+        "finished": true,
+        "startTime": 1689858950,
+        "finalTime": 1689858959
+    },
+    {
+        "id": "C6jshlNuBD",
+        "ranking": "OiCdafywDg",
+        "gameNumber": 4,
+        "account": "quemossabe@gmail.com",
+        "player1": {
+            "id": "asgmFhR4ta",
+            "name": "MA LIN",
+            "clubId": "xbEoyZqpZ9"
+        },
+        "player1Set": 3,
+        "player2": {
+            "id": "QhiNkz9svJ",
+            "name": "CAZUO MATSUMOTO",
+            "clubId": "9wFZEy1KXt"
+        },
+        "player2Set": 0,
+        "finished": true,
+        "startTime": 1689858952,
+        "finalTime": 1689858966
+    },
+    {
+        "id": "iGZbuU4OJd",
+        "ranking": "OiCdafywDg",
+        "gameNumber": 5,
+        "account": "quemossabe@gmail.com",
+        "player1": {
+            "id": "CbBhnZnfSp",
+            "name": "TIMO BOLL",
+            "clubId": "ULCitbCiOX"
+        },
+        "player1Set": 0,
+        "player2": {
+            "id": "QhiNkz9svJ",
+            "name": "CAZUO MATSUMOTO",
+            "clubId": "9wFZEy1KXt"
+        },
+        "player2Set": 3,
+        "finished": true,
+        "startTime": 1689858972,
+        "finalTime": 1689858981
+    },
+    {
+        "id": "eWZ2djTsXb",
+        "ranking": "OiCdafywDg",
+        "gameNumber": 6,
+        "account": "quemossabe@gmail.com",
+        "player1": {
+            "id": "yP8fPpcud7",
+            "name": "ALEXIS LEBRUN",
+            "clubId": "9wFZEy1KXt"
+        },
+        "player1Set": 3,
+        "player2": {
+            "id": "asgmFhR4ta",
+            "name": "MA LIN",
+            "clubId": "xbEoyZqpZ9"
+        },
+        "player2Set": 0,
+        "finished": true,
+        "startTime": 1689858973,
+        "finalTime": 1689858987
+    },
+    {
+        "id": "B9bev53xH8",
+        "ranking": "OiCdafywDg",
+        "gameNumber": 7,
+        "account": "quemossabe@gmail.com",
+        "player1": {
+            "id": "CbBhnZnfSp",
+            "name": "TIMO BOLL",
+            "clubId": "ULCitbCiOX"
+        },
+        "player1Set": 3,
+        "player2": {
+            "id": "GCuIUJJTTj",
+            "name": "TIAGO APOLONIO",
+            "clubId": "xbEoyZqpZ9"
+        },
+        "player2Set": 0,
+        "finished": true,
+        "startTime": 1689860347,
+        "finalTime": 1689860398
+    },
+    {
+        "id": "fIlrbB8R4v",
+        "ranking": "OiCdafywDg",
+        "gameNumber": 8,
+        "account": "quemossabe@gmail.com",
+        "player1": {
+            "id": "QhiNkz9svJ",
+            "name": "CAZUO MATSUMOTO",
+            "clubId": "9wFZEy1KXt"
+        },
+        "player1Set": 3,
+        "player2": {
+            "id": "GCuIUJJTTj",
+            "name": "TIAGO APOLONIO",
+            "clubId": "xbEoyZqpZ9"
+        },
+        "player2Set": 0,
+        "finished": true,
+        "startTime": 1689860695,
+        "finalTime": 1689861544
+    },
+    {
+        "id": "oYHGQIiHVe",
+        "ranking": "OiCdafywDg",
+        "gameNumber": 9,
+        "account": "quemossabe@gmail.com",
+        "player1": {
+            "id": "yP8fPpcud7",
+            "name": "ALEXIS LEBRUN",
+            "clubId": "9wFZEy1KXt"
+        },
+        "player1Set": 2,
+        "player2": {
+            "id": "GCuIUJJTTj",
+            "name": "TIAGO APOLONIO",
+            "clubId": "xbEoyZqpZ9"
+        },
+        "player2Set": 3,
+        "finished": true,
+        "startTime": 1689861559,
+        "finalTime": 1689862036
+    },
+    {
+        "id": "3RvbHNYUpD",
+        "ranking": "OiCdafywDg",
+        "gameNumber": 10,
+        "account": "quemossabe@gmail.com",
+        "player1": {
+            "id": "asgmFhR4ta",
+            "name": "MA LIN",
+            "clubId": "xbEoyZqpZ9"
+        },
+        "player1Set": 3,
+        "player2": {
+            "id": "GCuIUJJTTj",
+            "name": "TIAGO APOLONIO",
+            "clubId": "xbEoyZqpZ9"
+        },
+        "player2Set": 2,
+        "finished": true,
+        "startTime": 1689972092,
+        "finalTime": 1689972099
+    }
+];
+
+
+let classification = [];
+let map = new Map();
+listGame.forEach((item) => {
+    if (map.get(item.player1.id) == null) {
+        let player = new Object();
+        player.setPositives = item.player1Set;
+        player.setNegatives = item.player2Set;
+        if (item.player1Set > item.player2Set) {
+            player.wins = 1;
+            player.defeats = 0;
+        } else {
+            player.wins = 0;
+            player.defeats = 1;
+        }
+
+        map.set(item.player1.id, player);
+    } else {
+        map.get(item.player1.id).setPositives = map.get(item.player1.id).setPositives + item.player1Set;
+        map.get(item.player1.id).setNegatives = map.get(item.player1.id).setNegatives + item.player2Set;
+        if (item.player1Set > item.player2Set) {
+            map.get(item.player1.id).wins = map.get(item.player1.id).wins + 1;
+        } else {
+            map.get(item.player1.id).defeats = map.get(item.player1.id).defeats + 1;
+        }
+        map.get(item.player1.id).wins = map.get(item.player1.id).wins + 1;
+    }
+
+    if (map.get(item.player2.id) == null) {
+        let player = new Object();
+        player.setPositives = item.player2Set;
+        player.setNegatives = item.player1Set;
+        if (item.player2Set > item.player1Set) {
+            player.wins = 1;
+            player.defeats = 0;
+        } else {
+            player.wins = 0;
+            player.defeats = 1;
+        }
+        map.set(item.player2.id, player);
+    } else {
+        map.get(item.player2.id).setPositives = map.get(item.player2.id).setPositives + item.player2Set;
+        map.get(item.player2.id).setNegatives = map.get(item.player2.id).setNegatives + item.player1Set;
+        if (item.player2Set > item.player1Set) {
+            map.get(item.player2.id).wins = map.get(item.player2.id).wins + 1;
+        } else {
+            map.get(item.player2.id).defeats = map.get(item.player2.id).defeats + 1;
+        }
+    }
+
+
+});
+map.forEach((v, k) => {
+    console.log(k + " - setPro: " + v.setPositives + " - setContra: " + v.setNegatives + " - vitorias: " + v.wins + " - derrotas: " + v.defeats);
+});
+console.log(map);
+
+for (let item of map) {
+    console.log(item);
+}
+
+// let map = new Map();
+// map.set("name", "teruya");
+// console.log(map.get("name"));
+
+// const player = {
+//     playerId: "kkk",
+//     games: 2,
+//     wins: 3,
+//     defeats: 10,
+//     winsAverages: 0.1,
+//     setPositives: 3,
+//     setNegatives: 2,
+//     setAverages: 0.1,
+//     pointsPositives: 40,
+//     pointsNegatives: 10,
+//     pointsAverages: 0.1,
+// }
+
+// let p = new Map();
+// p.set("xxx", player);
+// console.log(p.get("xxx").defeats);
+
+
+// let atleta = new Object();
+// atleta.playerId = "hhh";
+
+// console.log(atleta.playerId);
+// console.log(atleta.x == null);
+
